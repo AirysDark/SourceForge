f213cea84d5714436890ca7242ae125f8261095c
AirysDark <megamanx2sonic@gmail.com>
2026-02-22 00:01:30 +1100

    ue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,12 @@
-
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="SourceForge API")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
